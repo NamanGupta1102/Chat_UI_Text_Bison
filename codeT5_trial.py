d70e64540b0bffa5634b4ae3448ab4af8129e12e
NamanGupta1102 <naman1102@gmail.com>
2024-03-26 20:11:11 +0530

    LLM_on_Streamlit
    Need to add the actual model

--- codeT5_trial.py
+++
@@ -1,6 +0,0 @@
-import simpleT5.simplet5.simplet5 as sp
-model = sp.SimpleT5()
-# model.from_pretrained(model_type="codet5", model_name="Salesforce/codet5-small")
-model.load_model("codet5", "https://drive.google.com/drive/folders/15vixFcdc9aOKxEJ-DPZC-_FmB71j65Wq?usp=drive_link")
-print(model.predict('eval(input("enter any equation"))'))
-print('hi')
